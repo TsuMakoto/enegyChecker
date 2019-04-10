e6936cb4845b212d9c5509848c7d9329f20fc71a
TsuMakoto <tsukamoto@fusic.co.jp>
2019-04-10 18:23:38 +0900

    ニコニコプレビュー画面表示処理

--- script/niconicoFunc.js
+++ script/niconicoFunc.js
@@ -2,14 +2,13 @@
  * 入力されている日毎のデータを連想配列で保存
  * key : 日付
  * value : 
- *      niconico-icon : 画像のURL,
+ *      niconico-icon : アイコンの右からの番号,
  *      comment : 一言コメント,
  *      reflection : 振り返り
  *
- *      データベースへの保存用配列
+ *      データベースへの保存用配列(にできると思われる)
  * */
-const NICONICO_DATA = {};
-
+var _niconicoData= {};
 
 /** 
  * 日付の上をマウスがホバーした時のイベントを登録
@@ -38,15 +37,21 @@ $(document).on('click', '.calendar td', function() {
 
     // TODO: NICONICO_DATAから現在の状態を取得し
     // previewNicoFuncで現在の状態をサイドバーに反映
-    previewNicoFunc();
+    previewNicoFunc($(this).text());
 });
 
+
+/** 
+ * アイコンをクリックした時に、アイコンが選択状態にする
+ * 先に選択されているアイコンは選択を解除する。
+ * */
 $(document).on('click', '.nico-icons img', function() {
     // 現在選択されているアイコンを解除
     $('.selected-icon').removeClass('selected-icon');
     // 選択された日付にアイコンを付与
     $(this).addClass('selected-icon');
 });
+
 
 /**
  * 保存ボタンが押された時に、値を更新する。
@@ -55,19 +60,26 @@ $(document).on('click', '.nico-icons img', function() {
 $(document).on('click', '#registed', function() {
     // 現在設定されているカレンダーの日付を取得
     const idxSelectedDate = $('.calendar td').index($('.selected-date'));
-    const dateNum = $('.calendar table td').eq(idxSelectedDate).text(); 
-    // 値の更新
-    NICONICO_DATA[dateNum] = {
-        'niconico-icon' : $('#nico-condition .selected-icon').attr('src'),
-        'comment' :  $('#day-comment').val(),
-        'reflection' : $('#reflection').val()
-    };
-    
+    if (idxSelectedDate !== -1) {
+        
+        const dateNum = $('.calendar table td').eq(idxSelectedDate).text(); 
+        // 値の更新
+        _niconicoData[dateNum] = {
+            'niconico-icon' : $('#nico-condition img').index($('.selected-icon')),
+            'comment' :  $('#day-comment').val(),
+            'reflection' : $('#reflection').val()
+        };
+    }
 });
+
 
 /** 
  * 現在のニコニコ機能の状態を表示
+ * @param {string} targetDate ニコニコ機能の配列の表示したい日付
+ *
  * */
-function previewNicoFunc() {
-    
+function previewNicoFunc(targetDate) {
+    const selectedDateNicoItem = _niconicoData[targetDate];
+    $('#nico-condition img').eq(selectedDateNicoItem["niconico-icon"]).addClass('selected-icon');
+
 }
